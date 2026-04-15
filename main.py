7afa7b55f9e72ffd6506397939f8c0e87816521f
martoff <martoff@ukr.net>
2026-04-15 12:45:46 +0300

    'Solution'

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import json as json_module
 from car.models import Car
 from car.serializers import CarSerializer
 
+
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     return json_module.dumps(serializer.data).encode("utf-8")
-
 
 
 def deserialize_car_object(json: bytes) -> Car:
